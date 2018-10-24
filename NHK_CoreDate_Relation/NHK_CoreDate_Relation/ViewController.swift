d112a978bc824ee8a7cd5c8e7834d469e4a4674e
Jayan <jayan@appoets.com>
2018-10-25 01:03:43 +0530

    Core data relation sample

---
+++ NHK_CoreDate_Relation/NHK_CoreDate_Relation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NHK_CoreDate_Relation
+//
+//  Created by CSS on 25/10/18.
+//  Copyright © 2018 CSS. All rights reserved.
+//https://hackernoon.com/core-data-relationships-d813ed66ba8c
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
